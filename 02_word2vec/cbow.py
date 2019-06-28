d19f7d4e861c8f319357798e241a1b7685768b28
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2019-06-28 14:41:23 +0900

    :art: Create base class for word2vec

--- 02_word2vec/cbow.py
+++ 02_word2vec/cbow.py
@@ -1,16 +1,12 @@
-import os
-from datetime import datetime
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.compat import v1 as tfv1
 
-from mixins import BoardRecorderMixin
-from word2vec import DistributedRepresentations
+from word2vec import Word2Vec
 
 
-class CBOW(BoardRecorderMixin):
-    """Accelerated CBOW model.
+class CBOW(Word2Vec):
+    """Wor2Vec by CBOW model.
 
     Attributes:
         words (list): List of words
@@ -20,65 +16,22 @@ class CBOW(BoardRecorderMixin):
             after call `train` method.
     """
 
-    model_file_name = 'model.chpt'
+    def get_incomes(self):
+        return self.get_contexts()
 
-    @classmethod
-    def create_from_text(cls, text):
-        """Create SimpleCBOW instance form text.
+    def get_labels(self):
+        return self.get_targets()
 
-        Args:
-            text (str): text to analyze
-
-        Returns:
-            SimpleCBOW: Instance crated
-        """
-        duplicate_words = text.lower().replace('.', ' .').split(' ')
-        words = list(set(duplicate_words))
-        corpus = [words.index(word) for word in duplicate_words]
-        return cls(words, corpus)
-
-    def __init__(self, words, corpus):
-        """Create SimpleCBOW instance form corpus.
-
-        Args:
-            words (list): List of words
-            corpus (list): Corpus
-        """
-        self.words = words
-        self.words_size = len(words)
-        self.corpus = corpus
-
-    def get_contexts(self):
-        return np.r_[
-            np.array([
-                self.corpus[i:(i-self.window_size*2 or len(self.corpus))]
-                for i in range(self.window_size)
-            ]),
-            np.array([
-                self.corpus[i:(i-self.window_size*2 or len(self.corpus))]
-                for i in range(self.window_size+1, self.window_size*2+1)
-            ]),
-        ].T
-
-    def get_target(self):
-        return np.array(self.corpus[self.window_size:-self.window_size])
-
-    @property
-    def data_size(self):
-        if not hasattr(self, '_data_size'):
-            self._data_size = len(self.corpus) - self.window_size * 2
-        return self._data_size
-
-    def fetch_batch(self, contexts, labels, epoch_i, batch_i, batch_size):
+    def fetch_batch(self, incomes, labels, epoch_i, batch_i, batch_size):
         if batch_size is None or self.data_size < batch_size:
             return (
-                contexts,
+                incomes,
                 labels,
                 self.data_size,
             )
         if not hasattr(self, 'cl'):
             self.cl = np.concatenate(
-                [contexts, labels.reshape((-1, 1))],
+                [incomes, labels.reshape((-1, 1))],
                 axis=1,
             )
         if batch_i == 0:
@@ -98,28 +51,12 @@ class CBOW(BoardRecorderMixin):
             window_size (int): Window size
             hidden_size (int): Dimension of a vector encoding the words
         """
-        if hasattr(self, '_data_size'):
-            del self._data_size
-        self.window_size = window_size
-        self.hidden_size = hidden_size
-        self.learning_rate = tfv1.placeholder(tf.float32, name='learning_rate')
+        self.build_model_params(window_size, hidden_size)
 
-        self.W_in = tf.Variable(
-            tf.random.uniform([self.words_size, self.hidden_size],
-                              -1.0, 1.0, dtype=tf.float32),
-            dtype=tf.float32,
-            name='W_in',
-        )
-        self.W_out = tf.Variable(
-            tf.random.uniform([self.hidden_size, self.words_size],
-                              -1.0, 1.0, dtype=tf.float32),
-            dtype=tf.float32,
-            name='W_out',
-        )
-        contexts = tfv1.placeholder(
+        incomes = tfv1.placeholder(
             tf.int32,
             shape=(None, self.window_size*2),
-            name='contexts',
+            name='incomes',
         )
         labels = tfv1.placeholder(
             tf.int32,
@@ -127,14 +64,14 @@ class CBOW(BoardRecorderMixin):
             name='labels',
         )
         batch_size = tfv1.placeholder(tf.float32, name='batch_size')
-        self.contexts = contexts
+        self.incomes = incomes
         self.labels = labels
         self.batch_size = batch_size
 
         cee = tf.reduce_mean(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
                 labels=labels,
-                logits=self._build_cbow_output(contexts),
+                logits=self._build_cbow_output(incomes),
             ), name='CEE',
         )
         self.training_op = self._build_optimize_graph(cee)
@@ -144,8 +81,8 @@ class CBOW(BoardRecorderMixin):
         optimizer = tfv1.train.AdamOptimizer(learning_rate=self.learning_rate)
         return optimizer.minimize(cee)
 
-    def _build_cbow_output(self, contexts):
-        b, w = contexts.shape
+    def _build_cbow_output(self, incomes):
+        b, w = incomes.shape
         v, h = self.W_in.shape.as_list()
         return tf.matmul(
             tf.reshape(
@@ -155,7 +92,7 @@ class CBOW(BoardRecorderMixin):
                             tf.reshape(
                                 tf.gather(
                                     self.W_in,
-                                    tf.reshape(contexts, [-1]),
+                                    tf.reshape(incomes, [-1]),
                                 ), [-1, w, h],
                             ), [1, 0, 2],
                         ), [w, -1],
@@ -164,62 +101,3 @@ class CBOW(BoardRecorderMixin):
             ),
             self.W_out,
         )
-
-    def train(self, log_dir=None, max_epoch=10000, learning_rate=0.001,
-              batch_size=None, interval_sec=300, restore_step=None):
-        """Train CBOW model.
-
-        Args:
-            log_dir (str): Log directory where log and model is saved.
-            max_epoch (int): Size of epoch
-            learning_rate (float): Learning rate
-            batch_size (int): Batch size when using mini-batch descent method.
-                If specifying a size larger then learning data or `None`,
-                using batch descent.
-            interfal_sec (float): Specify logging time interval in seconds.
-                Default by 300.
-            restore_step (int): When you specify this argument, this mixin
-                resotres model for specified step.
-        """
-        if log_dir is None:
-            log_dir = os.path.join(os.path.dirname(__file__),
-                                   'tf_logs',
-                                   datetime.utcnow().strftime('%Y%m%d%H%M%S'))
-        if batch_size is None:
-            n_batches = 1
-        else:
-            n_batches = int(np.ceil(self.data_size / batch_size))
-        with self.open_writer(log_dir) as writer:
-            with self.open_session(interval_sec=interval_sec,
-                                   per_step=n_batches,
-                                   restore_step=restore_step) as sess:
-                contexts = self.get_contexts()
-                labels = self.get_target()
-                self.word_reps = DistributedRepresentations(
-                    self.words,
-                    sess.run(self.W_in))
-                step = restore_step or 0
-                if restore_step is None:
-                    writer.add_summary(
-                        self.los_summary.eval(
-                            feed_dict={self.contexts: contexts[:batch_size],
-                                       self.labels: labels[:batch_size]},
-                        ),
-                        step,
-                    )
-                for epoch_i in range(step // self.data_size, max_epoch):
-                    for batch_i in range(n_batches):
-                        c, l, b = self.fetch_batch(contexts, labels,
-                                                   epoch_i, batch_i,
-                                                   batch_size)
-                        fd = {
-                            self.contexts: c,
-                            self.labels: l,
-                            self.batch_size: b,
-                            self.learning_rate: learning_rate,
-                        }
-                        sess.run(self.training_op, feed_dict=fd)
-                        self.record(sess, writer, step, feed_dict=fd)
-                        step += 1
-                    self.word_reps.vecs = sess.run(self.W_in)
-                self.record(sess, writer, step, feed_dict=fd, force_write=True)
--- 02_word2vec/word2vec.py
+++ 02_word2vec/word2vec.py
@@ -1,4 +1,12 @@
+import os
+from collections import Counter
+from datetime import datetime
+
 import numpy as np
+import tensorflow as tf
+from tensorflow.compat import v1 as tfv1
+
+from mixins import BoardRecorderMixin
 
 
 class DistributedRepresentations:
@@ -84,3 +92,164 @@ class DistributedRepresentations:
                 if top <= count:
                     print()
                     break
+
+
+class Word2Vec(BoardRecorderMixin):
+    """Base class for wor2vec.
+
+    Attributes:
+        words (list): List of words
+        words_size (int): Size of `words`
+        corpus (list): Corpus
+        word_vectors (:obj:`WordVectors`): Results of model. You can reforence
+            after call `train` method.
+    """
+
+    model_file_name = 'model.chpt'
+
+    @classmethod
+    def create_from_text(cls, text):
+        """Create SimpleCBOW instance form text.
+
+        Args:
+            text (str): text to analyze
+
+        Returns:
+            SimpleCBOW: Instance crated
+        """
+        duplicate_words = text.lower().replace('.', ' .').split(' ')
+        words = list(set(duplicate_words))
+        corpus = [words.index(word) for word in duplicate_words]
+        return cls(words, corpus)
+
+    def __init__(self, words, corpus):
+        """Create SimpleCBOW instance form corpus.
+
+        Args:
+            words (list): List of words
+            corpus (list): Corpus
+        """
+        self.words = words
+        self.words_size = len(words)
+        self.corpus = corpus
+        counter = Counter(corpus)
+        self.counts = np.array([counter[i] for i in range(self.words_size)])
+
+    def get_contexts(self):
+        return np.r_[
+            np.array([
+                self.corpus[i:(i-self.window_size*2 or len(self.corpus))]
+                for i in range(self.window_size)
+            ]),
+            np.array([
+                self.corpus[i:(i-self.window_size*2 or len(self.corpus))]
+                for i in range(self.window_size+1, self.window_size*2+1)
+            ]),
+        ].T
+
+    def get_targets(self):
+        return np.array(self.corpus[self.window_size:-self.window_size])
+
+    @property
+    def data_size(self):
+        if not hasattr(self, '_data_size'):
+            self._data_size = len(self.corpus) - self.window_size * 2
+        return self._data_size
+
+    def build_model_params(self, window_size, hidden_size):
+        if hasattr(self, '_data_size'):
+            del self._data_size
+        self.window_size = window_size
+        self.hidden_size = hidden_size
+        self.learning_rate = tfv1.placeholder(tf.float32, name='learning_rate')
+
+        self.W_in = tf.Variable(
+            tf.random.uniform([self.words_size, self.hidden_size],
+                              -1.0, 1.0, dtype=tf.float32),
+            dtype=tf.float32,
+            name='W_in',
+        )
+        self.W_out = tf.Variable(
+            tf.random.uniform([self.hidden_size, self.words_size],
+                              -1.0, 1.0, dtype=tf.float32),
+            dtype=tf.float32,
+            name='W_out',
+        )
+
+    def build_model(self, window_size=1, hidden_size=5,
+                    ns_count=0, ns_exponent=0.75):
+        """Build CBOW graph.
+
+        Args:
+            window_size (int): Window size
+            hidden_size (int): Dimension of a vector encoding the words
+        """
+        raise NotImplementedError
+
+    def get_incomes(self):
+        raise NotImplementedError
+
+    def get_labels(self):
+        raise NotImplementedError
+
+    def fetch_batch(self, incomes, labels, epoch_i, batch_i, batch_size):
+        raise NotImplementedError
+
+    def train(self, log_dir=None, max_epoch=10000, learning_rate=0.001,
+              batch_size=None, interval_sec=300, restore_step=None):
+        """Train model.
+
+        Args:
+            log_dir (str): Log directory where log and model is saved.
+            max_epoch (int): Size of epoch
+            learning_rate (float): Learning rate
+            batch_size (int): Batch size when using mini-batch descent method.
+                If specifying a size larger then learning data or `None`,
+                using batch descent.
+            interfal_sec (float): Specify logging time interval in seconds.
+                Default by 300.
+            restore_step (int): When you specify this argument, this mixin
+                resotres model for specified step.
+        """
+        if log_dir is None:
+            log_dir = os.path.join(os.path.dirname(__file__),
+                                   'tf_logs',
+                                   datetime.utcnow().strftime('%Y%m%d%H%M%S'))
+        if batch_size is None:
+            n_batches = 1
+        else:
+            n_batches = int(np.ceil(self.data_size / batch_size))
+        with self.open_writer(log_dir) as writer:
+            with self.open_session(interval_sec=interval_sec,
+                                   per_step=n_batches,
+                                   restore_step=restore_step) as sess:
+                incomes = self.get_incomes()
+                labels = self.get_labels()
+                self.word_reps = DistributedRepresentations(
+                    self.words,
+                    sess.run(self.W_in))
+                step = restore_step or 0
+                if restore_step is None:
+                    writer.add_summary(
+                        self.los_summary.eval(
+                            feed_dict={self.incomes: incomes[:batch_size],
+                                       self.labels: labels[:batch_size]},
+                        ),
+                        step,
+                    )
+                for epoch_i in range(step // self.data_size, max_epoch):
+                    for batch_i in range(n_batches):
+                        c, l, b = self.fetch_batch(incomes, labels,
+                                                   epoch_i, batch_i,
+                                                   batch_size)
+                        fd = {
+                            self.incomes: c,
+                            self.labels: l,
+                            self.batch_size: b,
+                            self.learning_rate: learning_rate,
+                        }
+                        sess.run(self.training_op, feed_dict=fd)
+                        self.record(sess, writer, step, feed_dict=fd)
+                        step += 1
+                    self.word_reps.vecs = sess.run(self.W_in)
+                self.record(sess, writer, step, feed_dict=fd, force_write=True)
